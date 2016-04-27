de37d15a74473ef59f7ddcdad3a3da82a0134e46
lijiehua <41830859@qq.com>
2016-04-27 19:26:38 +0800

    web

--- pivot-core/src/main/java/com/edoyun/pivot/App.java
+++ pivot-core/src/main/java/com/edoyun/pivot/App.java
@@ -1,13 +1,28 @@
 package com.edoyun.pivot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("slf4j-log4j debug message");
+		}
+		if (logger.isInfoEnabled()) {
+			logger.info("slf4j-log4j info message");
+		}
+		if (logger.isTraceEnabled()) {
+			logger.trace("slf4j-log4j trace message");
+		}
+	}
 }
